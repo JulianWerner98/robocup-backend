b0e5531d30681aa87910b4e5adc6103b22311c38
Julian Werner <info@werner-julian.de>
2022-09-12 14:52:08 +0200

    feat: config action

--- src/config/config.validator.ts
+++ src/config/config.validator.ts
@@ -2,7 +2,7 @@ import * as Joi from "joi";
 
 export const validationSchema = Joi.object({
    PORT: Joi.number().default(3000),
-   MONGO_URI: Joi.string().default('mongodb://localhost:27017/robocup'),
+   MONGO_URI: Joi.string().default('mongodb://mongoDB:27017/robocup'),
    KEYCLOAK_URI: Joi.string().default('http://localhost:8080/auth'),
    KEYCLOAK_REALM: Joi.string().default('RoboCup'),
    KEYCLOAK_CLIENT_ID: Joi.string().default('app-backend'),
